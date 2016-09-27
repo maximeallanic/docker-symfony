fb904f71db04895bf80c8381b36384ef7a5e7a3c
Maxime Allanic <maxime@allanic.me>
2016-09-27 15:40:36 +0200

    [FIX] Nginx PHP link

--- bin/console.sh
+++ bin/console.sh
@@ -1,4 +1,12 @@
+# @Author: Maxime Allanic <mallanic>
+# @Date:   26/09/2016
+# @Email:  maxime@allanic.me
+# @Last modified by:   mallanic
+# @Last modified time: 26/09/2016
+
+
+
 #!/usr/bin/env sh
 
-DOCKER_PHP_ID=$(docker ps | grep docker_php | awk '{print $1}')
-docker exec -it $DOCKER_PHP_ID su www-data -c "app/console $@"
+DOCKER_PHP_ID=$(docker ps | grep 9000/tcp | awk '{print $1}')
+docker exec -it $DOCKER_PHP_ID su www-data -c "bin/console $@"
